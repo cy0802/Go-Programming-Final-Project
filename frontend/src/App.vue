06603b3e97ff18f075a7bf0dd8f52e4d0b401b9e
cy0802 <amyyoyo0802@gmail.com>
2024-12-12 12:08:05 +0800

    basic functionality done

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -46,13 +46,16 @@
 import { onMounted, ref } from 'vue';
 import { useDate } from 'vuetify'
 import EventDialog from './components/EventDialog.vue';
+import * as eventService from './services/event.js'
+
 const adapter = useDate();
 const events = ref([]);
 const selectedEvent = ref({});
 const dialog = ref(false);
 
-function addEvent(name, start, end, diary = '') {
+function addEvent(id, name, start, end, diary = '') {
   events.value.push({ 
+    id: id,
     title: name,
     start: start,
     end: end,
@@ -68,9 +71,24 @@ function openDialog(event) {
 }
 
 function updateEvent(updatedEvent, newEvent) {
-  console.log("updateEvent called");
-  console.log(updatedEvent);
-  console.log(newEvent);
+  const formattedDate = new Date(updatedEvent.start).toISOString().split('T')[0];
+  if (newEvent) {
+    try {
+      eventService.createEvent({ title: updatedEvent.title, date: formattedDate, diary: updatedEvent.diary });
+      addEvent(events.value.length + 1, updatedEvent.title, updatedEvent.start, updatedEvent.start, updatedEvent.diary);
+    } catch (error) {
+      console.error("Error creating event:", error);
+    }
+  } else {
+    try {
+      eventService.updateEvent(updatedEvent);
+      const index = events.value.findIndex(event => event.id === selectedEvent.value.id);
+      events.value[index].title = updatedEvent.title;
+      events.value[index].diary = updatedEvent.diary;
+    } catch (error) {
+      console.error("Error updating event:", error);
+    }
+  }
 }
 
 function addNewEvent() {
@@ -78,10 +96,15 @@ function addNewEvent() {
   dialog.value = true;
 }
 
-onMounted(() => {
-  addEvent('Event 2', adapter.startOfDay(new Date('2024-12-01')), adapter.endOfDay(new Date('2024-12-01')), "hello, world");
-  addEvent('Event 4', adapter.startOfDay(new Date('2024-12-01')), adapter.endOfDay(new Date('2024-12-01')));
-  addEvent('Event 2024', adapter.startOfDay(new Date('2024-12-02')), adapter.endOfDay(new Date('2024-12-02')));
+onMounted(async () => {
+  try {
+    var fetchedEvent = await eventService.getEvents();
+    fetchedEvent.forEach(event => {
+      addEvent(event.id, event.title, adapter.startOfDay(new Date(event.date)), adapter.endOfDay(new Date(event.date)), event.diary);
+    });
+  } catch (error) {
+    console.error("Error fetching events:", error);
+  }
 });
 
 </script>
